42690100d0326e2ec8300b57d1adcfbac5f13fbd
baboyiban <chl11wq12@gmail.com>
2025-06-09 14:20:27 +0900

    init: barcode-scanner

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -8,10 +8,6 @@ fn greet(name: &str) -> String {
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_barcode_scanner::init())
-        .plugin(
-            // tauri_plugin_opener::init(),
-            tauri_plugin_barcode_scanner::init(),
-        )
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
